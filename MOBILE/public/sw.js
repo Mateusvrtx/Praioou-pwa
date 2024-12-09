8b14a19afbec07354d2081dfb1a060463f728606
Mateusvrtx <mateus.oliveira.vrtx@gmail.com>
2024-12-09 08:55:46 -0300

    PWA ARRUMADO

--- MOBILE/public/sw.js
+++ MOBILE/public/sw.js
@@ -1,19 +1,22 @@
 self.addEventListener('install', (event) => {
+    console.log('Instalando Service Worker...');
     event.waitUntil(
         caches.open('v1').then((cache) => {
+            console.log('Abrindo cache...');
             return cache.addAll([
                 '/',
-                '/index.html',
                 '/login/assets/style.css',
                 '/login/assets/script.js',
-                '/icons/icon512_maskable.png',
-                '/icons/icon512_rounded.png',
+                '/icons/bolinhapraioou.svg',
             ]);
+        }).catch((error) => {
+            console.error('Erro ao adicionar ao cache:', error);
         })
     );
 });
 
 self.addEventListener('fetch', (event) => {
+    console.log('Interceptando fetch para:', event.request.url);
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
